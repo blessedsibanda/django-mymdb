e7a625aeac56f81d2145e360b5f48d9649a412d8
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-04-30 12:05:23 +0200

    individual movie pages

--- django/core/models.py
+++ django/core/models.py
@@ -20,5 +20,8 @@ class Movie(models.Model):
     runtime = models.PositiveIntegerField()
     website = models.URLField(blank=True)
 
+    class Meta:
+        ordering = ('-year', 'title')
+
     def __str__(self):
         return '{} ({})'.format(self.title, self.year)
--- django/core/urls.py
+++ django/core/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'core'
 urlpatterns = [
-    path('movies', views.MovieList.as_view(),name='MovieList')
+    path('movies', views.MovieList.as_view(), name='MovieList'),
+    path('movies/<int:pk>', views.MovieDetail.as_view(), name='MovieDetail'),
 ]
